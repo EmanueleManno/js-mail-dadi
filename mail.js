54ed8c986190252329f39c57dbdb87eaf59c56a3
EmanueleManno <mannus1994@gmail.com>
2023-05-31 17:17:21 +0200

    Email Validation

--- mail.js
+++ mail.js
@@ -2,7 +2,8 @@
 console.log('JS OK');
 
 //RECUPERO L'ELEMENTO DAL DOM:
-const mailAnswer = document.getElementById('mail-answer');
+const mailAnswerOk = document.getElementById('mail-answer-ok');
+const mailAnswerNo = document.getElementById('mail-answer-no');
 
 //CREO LA LISTA DI MAIL:
 const mailList = ['emanuele.manno@gmail.com', 'marco.lanci@gmail.com', 'marco.baroncini@gmail.com', 'marius.minia@gmail.com'];
@@ -12,3 +13,15 @@ console.table(mailList);
 const mailInput = prompt('Dimmi una mail valevole per poter accedere!').trim();
 console.log(mailInput);
 
+//VERIFICO CHE SIA PRESENTE ALL'INTERNO DELLA LISTA:
+for (let i = 0; i < 4; i++) {
+console.log(mailList[i]);
+
+    if (mailInput == mailList[i]) {
+    console.log('Benvenuto ' + mailInput);
+    mailAnswerOk.innerText = ('Benvenuto ' + mailInput);  
+}
+    else (mailInput != mailList[i]) 
+    console.log('NON PUOI ACCEDERE FURFANTE');
+    mailAnswerNo.innerText = ('NON PUOI ACCEDERE FURFANTE');
+}
